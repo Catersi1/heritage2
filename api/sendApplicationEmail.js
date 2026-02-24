84172f4d4cae2e29439fa743426e4cf75bde0de3
Heritage Admin <admin@casasoklahoma.com>
2026-02-25 01:12:39 +0800

    Fix Resend from address

--- api/sendApplicationEmail.js
+++ api/sendApplicationEmail.js
@@ -76,7 +76,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        from: 'Heritage Housing <onboarding@resend.dev>',
+        from: 'onboarding@resend.dev',
         to: ADMIN_EMAIL,
         subject: subject,
         html: htmlContent
